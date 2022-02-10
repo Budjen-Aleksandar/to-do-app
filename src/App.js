a455615f4aa0dac06d8a81019bb55d64c97ae7e9
Ognjen Covilo <ognjen@hurricane.studio>
2022-02-10 18:42:27 +0100

    comments on initial work, Form component refactor

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 // import logo from './logo.svg';
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 // importing componenets!
 import Form from './components/Form';
 import TodoList from './components/TodoList';
 
 function App() {
+  // also I guess state is missing here
+  const [todos, setTodos] = useState([])
+  // changing this line as per comment on the Form component
+  const addTodo = (todo) => {
+    setTodos([
+      ...todos,
+      todo
+    ])
+  }
   return (
     <div className='App'>
       <header>
       <h1>To-Do-List</h1>
       </header>
-      <Form />
+      <Form addTodo={addTodo}/>
+      {/*probably missing here*/}
+      <TodoList setTodos={setTodos} todos={todos} />
     </div>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,17 +1,18 @@
 /* eslint-disable array-callback-return */
 import React from 'react'
 import Todo from './Todo';
-
+// The same changes and guidelines apply to this component as well as for Form component,
+// I leave it up to you to think about it and refactor this with that in mind (and for Todo component as well)
 const TodoList = ({ todos, setTodos }) => {
     return (
         <div className="todo-container">
       <ul className="todo-list">
        {todos.map((todo) => {
-         return <Todo 
-         setTodos={setTodos} 
-         todos={todos} 
-         key={todo.id} 
-         text={todo.text} 
+         return <Todo
+         setTodos={setTodos}
+         todos={todos}
+         key={todo.id}
+         text={todo.text}
          todo={todo}/>
        })}
       </ul>
