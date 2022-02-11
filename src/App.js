cc3d42a39d6371feb355807b2899c0047cda5756
Budjen-Aleksandar <balexandar42@gmail.com>
2022-02-11 11:19:40 +0100

    izmjena 2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
+/* eslint-disable no-unused-vars */
 // import logo from './logo.svg';
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 // importing componenets!
 import Form from './components/Form';
 import TodoList from './components/TodoList';
 
 function App() {
+  const [textInput, setTextInput] = useState('');
+  const [todos, setTodos] = useState([]);
+
   return (
     <div className='App'>
       <header>
-      <h1>To-Do-List</h1>
+      <h1>To-Do-List </h1>
       </header>
-      <Form />
+      <Form textInput={textInput} todos={todos} setTodos={setTodos} setTextInput={setTextInput}/>
+      <TodoList setTodos={setTodos} todos={todos}/>
     </div>
   );
+
+
 }
 
 export default App;
